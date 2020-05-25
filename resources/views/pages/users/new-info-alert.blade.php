56e1214a1dfb58c549752b383562d820dc7ff3f5
Marina Kuklina <nelmare@yandex.ru>
2020-05-25 12:43:08 +0300

    Change info-alert

--- resources/views/pages/users/new-info-alert.blade.php
+++ resources/views/pages/users/new-info-alert.blade.php
@@ -7,7 +7,7 @@
                     <h1 class="font-weight-bold text-center reset-title">Восстановление пароля</h1>
                 </div>
                 <div class="alert alert-primary text-center mx-auto col-lg-8" role="alert">
-                    На ваш ящик выслана новая инструкция по восстановлению пароля.
+                    На Вашу почту ... выслана новая инструкция по восстановлению пароля.
                 </div>
                 <div class="text-center mx-auto">
                     <a class="text-decoration-none text-uppercase btn button-login-return"
