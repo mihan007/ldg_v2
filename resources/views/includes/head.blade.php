971f0e5ab3ee019bc37e685d10848144885e2439
Marina Kuklina <nelmare@yandex.ru>
2020-05-25 15:33:23 +0300

    Add dropdown menu

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,3 +4,6 @@
 <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 <link rel="preload" href="/fonts/Malgun-Gothic29381.woff2" as="font">
 <script src="{{ mix('js/app.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
--- resources/views/includes/header-app.blade.php
+++ resources/views/includes/header-app.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-dark bg-grey">
         <a href="#" class="navbar-brand ">
             <img class="align-top" src="/img/logo.png" height="25" alt="Лидогенератор">
-            <span class="align-items-baseline text-uppercase">Лидогенератор</span>
+            <span class="align-items-baseline text-uppercase project-title">Лидогенератор</span>
         </a>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
@@ -33,16 +33,26 @@
                     </div>
                 </div>
             </div>
-            <form class="form-inline">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                    <div class="input-group-append">
-                        <button type="button" class="btn btn-secondary"><i class="fa fa-search"></i></button>
+{{--            <form class="form-inline">--}}
+{{--                <div class="input-group">--}}
+{{--                    <input type="text" class="form-control" placeholder="Search">--}}
+{{--                    <div class="input-group-append">--}}
+{{--                        <button type="button" class="btn btn-secondary"><i class="fa fa-search"></i></button>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </form>--}}
+            <div class="navbar-nav login-wrapper">
+                <i class="fa fa-user-o"></i>
+                <div class="nav-item dropdown">
+
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Messages</a>
+                    <div class="dropdown-menu">
+                        <a href="#" class="dropdown-item">Inbox</a>
+                        <a href="#" class="dropdown-item">Sent</a>
+                        <a href="#" class="dropdown-item">Drafts</a>
                     </div>
                 </div>
-            </form>
-            <div class="navbar-nav">
-                <a href="#" class="nav-item nav-link">Login</a>
+
             </div>
         </div>
     </nav>
