1f605877d2b2ec85828952e888fc40654a69a10d
Mikhail Kuklin <mihan007@ya.ru>
2020-06-09 12:09:04 +0400

    Fix set of issues: 1, 3, 4
    https://paradox.myjetbrains.com/youtrack/issue/LDG-213#focus=streamItem-4-750.0-0

--- resources/js/magic-menu.js
+++ resources/js/magic-menu.js
@@ -2,17 +2,27 @@ $(document).on('click', function (event) {
     let $this = $(event.target)
     $('.nav-item').removeClass('show')
     $('.dropdown-menu').removeClass('show')
-    if ($this.closest('.navbar-nav').length > 0) {
-        $this.closest('.magic-menu').addClass('show');
-        $this.closest('.magic-menu').find('.dropdown-menu').addClass('show');
-        if ($(window).width()<693) {
+    let switchCondition = $this.closest('.navbar-nav').length > 0
+    switchCondition = switchCondition && !$this.closest('.navbar-nav').hasClass('sidenav')
+    if (switchCondition) {
+        $this.closest('.magic-menu').addClass('show')
+        $this.closest('.magic-menu').find('.dropdown-menu').addClass('show')
+        if ($(window).width() < 693) {
             $('.scrollmenu').css('overflow', 'visible')
             $('.magic-hide').hide()
         }
     } else {
-        if ($(window).width()<693) {
+        if ($(window).width() < 693) {
             $('.scrollmenu').css('overflow', 'auto')
             $('.magic-hide').show()
         }
+    }
+})
+$(window).resize(function () {
+    if ($(window).width() < 693) {
+        $('.scrollmenu').css('overflow', 'auto')
+    } else {
+        $('.scrollmenu').css('overflow', 'visible')
+        $('.magic-hide').show()
     }
 })
--- resources/js/sorting.js
+++ resources/js/sorting.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    $.fn.dataTableExt.afnFiltering.push(
-        function( oSettings, aData, iDataIndex ) {
-            var row = oSettings.aoData[iDataIndex].nTr;
-            return $(row).hasClass('template') ? false : true;
-        }
-    );
     window.table = $('.company-grid').DataTable({
         fixedHeader: {
             headerOffset: 52
@@ -13,20 +7,20 @@ $(document).ready(function () {
         searching: false,
         info: false,
         autoWidth: false,
-        // columns: [
-        //     { "width": "250px" },
-        //     { "width": "67px" },
-        //     { "width": "80px" },
-        //     { "width": "91px" },
-        //     { "width": "91px" },
-        //     { "width": "100px" },
-        //     { "width": "91px" },
-        //     { "width": "91px" },
-        //     { "width": "91px" },
-        //     { "width": "91px" },
-        //     { "width": "91px" },
-        //     { "width": "91px" }
-        // ]
-    });
-    $('.dataTables_length').addClass('bs-select');
-});
+        columns: [
+            { 'orderSequence': ['asc', 'desc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+            { 'orderSequence': ['desc', 'asc'] },
+        ]
+    })
+    $('.dataTables_length').addClass('bs-select')
+})
