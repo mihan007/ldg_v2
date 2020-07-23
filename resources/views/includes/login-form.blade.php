6cfb7c508e4b38fe41d78326d8d4fa42da22974e
Marina Kuklina <nelmare@yandex.ru>
2020-07-23 12:02:22 +0400

    Adjust profile page

--- resources/views/includes/login-form.blade.php
+++ resources/views/includes/login-form.blade.php
@@ -5,8 +5,8 @@
             <span class="input-group-text"><i class="fa fa-envelope"></i></span>
         </div>
         <input class="form-control" type="email" id="input-email" placeholder="Email" required>
-        <div class="invalid-feedback login-email-invalid-feedback">Пожалуйста, введите правильный адрес email</div>
     </div>
+    <div class="invalid-feedback login-email-invalid-feedback">Пожалуйста, введите правильный адрес email</div>
 </div>
 <div class="form-group">
     <label class="sr-only" for="input-password">Password</label>
@@ -15,8 +15,8 @@
             <span class="input-group-text"><i class="fa fa-lock"></i></span>
         </div>
         <input class="form-control" type="password" id="input-password" placeholder="Пароль" required>
-        <div class="invalid-feedback login-password-invalid-feedback">Пожалуйста, введите верный пароль</div>
     </div>
+    <div class="invalid-feedback login-password-invalid-feedback">Пожалуйста, введите верный пароль</div>
 </div>
 <div class="form-group form-items d-flex justify-content-between">
     <label class="form-check-label">
